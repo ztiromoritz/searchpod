6edddb200964c69e80d0866f8c8e543b24ddb928
Moritz Rebbert <moritz.rebbert@udo.edu>
2024-01-08 00:33:37 +0100

    initial commit 🍞

---
+++ build.sh
@@ -0,0 +1,7 @@
+# TODO: 
+# - [ ] fetch external pods offline and generate a new pod
+#         -> index.js curls the data 
+#         -> simplest templating ever
+#         -> template.html
+# - [ ] put personal link list in a separate text file
+# - sync this via ftp/rsync/etc
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+npx live-server . --open=/prototype/
